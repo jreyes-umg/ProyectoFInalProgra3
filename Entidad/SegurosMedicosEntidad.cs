149d2c5d0ad6941fbc1ea61a1c3894fcfe32b13d
Cesar Villalta <cvillaltaa@miumg.edu.gt>
2026-05-20 19:39:54 -0600

    entidad seguros medicos

---
+++ Entidad/SegurosMedicosEntidad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    internal class SegurosMedicosEntidad
+    {
+
+        public int CodigoSeguro { get; set; }
+        public string NombreSeguro { get; set; }
+        public string TipoSeguro { get; set; }
+        public decimal PorcentajeCobertura { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public decimal MontoMaximo { get; set; }
+        public bool Estado { get; set; }
+        public string UsuarioSistema { get; set; }
+        public DateTime FechaSistema { get; set; }
+        public DateTime HoraSistema { get; set; }
+
+
+    }
+}
