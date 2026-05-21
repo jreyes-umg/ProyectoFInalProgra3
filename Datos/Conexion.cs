f54323913cb6fcf3d3c999c1c13226dfaef63b8c
juand <juand@RARINROCKSTAR>
2026-05-20 18:46:02 -0600

    Agregada la cadena de conexion y los paquetes necesarios

---
+++ Datos/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Datos
+{
+    public class ConexionDatos
+    {
+        private string CadenaConexion = ConfigurationManager.ConnectionStrings["CadenaConexionDB"].ConnectionString;
+
+        public SqlConnection MtdConexion()
+        {
+            return new SqlConnection(CadenaConexion);
+        }
+
+    }
+}
